7af1b9ef399d8201931badcf974c7465e60b620c
Demetris Paschalides <dpasch01@cs.ucy.ac.cy>
2018-09-16 10:58:48 +0300

    Added a 'Register' button.

--- www/js/index.js
+++ www/js/index.js
@@ -1,133 +1,220 @@
-$(document).ready(function(){
-    
-    navigator.geolocation.watchPosition(geolocationSuccess, geolocationError, {
-        enableHighAccuracy: true
-    });
 
-    $('.camera-btn').click(function(){
-        navigator.camera.getPicture(cameraSuccess, cameraError, {
-            quality: 25,
-            destinationType: Camera.DestinationType.DATA_URL
-        });
+$('.camera-btn').click(function(){
+    navigator.camera.getPicture(cameraSuccess, cameraError, {
+        quality: 25,
+        destinationType: Camera.DestinationType.DATA_URL
     });
+});
+
+$('.emergency-btn').click(function(){
+    swal({
+        title: 'Are you in danger?',
+        text: "By confirming this message an emergency help request will be made for you.",
+        type: 'warning',
+        showCancelButton: true,
+        confirmButtonClass: 'btn btn--stripe btn--radius blue',
+        cancelButtonClass: 'btn btn--stripe btn--radius red',
+        confirmButtonText: 'I\'m in danger!'
+        }).then((result) => {
+        if (result.value) {
+            emergencyRequest();
+        }
+        })
+});
     
-    $('.emergency-btn').click(function(){
+$('.register-btn').click(function(){
+    console.log(localStorage.getItem("isRegistered"));
+
+    if(localStorage.getItem("isRegistered") == null){
         swal({
-            title: 'Are you in danger?',
-            text: "By confirming this message an emergency help request will be made for you.",
-            type: 'warning',
+            title: 'Welcome to ProlepSYS',
+            text: "In order for us to inform you for a near danger we require your phone number. Would you be kind enough to provide it to us?",
+            type: 'question',
             showCancelButton: true,
             confirmButtonClass: 'btn btn--stripe btn--radius blue',
-            cancelButtonClass: 'btn btn--stripe btn--radius red',
-            confirmButtonText: 'I\'m in danger!'
-          }).then((result) => {
+            cancelButtonClass: 'btn btn--stripe btn--radius black',
+            confirmButtonText: 'Of course',
+            cancelButtonText: 'Maybe later'
+        }).then((result) => {
             if (result.value) {
-                emergencyRequest();
+                swal({
+                    title: 'Fill it in.',
+                    text: 'Type your phone number in order to receive near danger notifications',
+                    input: 'tel',
+                    showCancelButton: true,
+                    inputPlaceholder: 'e.g +35799556677',
+                    confirmButtonClass: 'btn btn--stripe btn--radius blue',
+                    cancelButtonClass: 'btn btn--stripe btn--radius black',
+                    allowOutsideClick: false
+            }).then((result) => {
+                registerUser(result.value);
+            });
             }
-          })
-    });
-        
-    function geolocationSuccess(currentLocation) {
-        console.log(currentLocation);
-        if(currentLocation!=null){
-            localStorage.setItem('currentLocation', JSON.stringify({
-                "lat": currentLocation.coords.latitude,
-                "lon": currentLocation.coords.longitude,
-            }));
+        });         
+    }
+  
+});
 
-            /*
+function geolocationSuccess(currentLocation) {
+    console.log(currentLocation);
+    if(currentLocation!=null){
+        updateCurrentLocation(currentLocation);
+    }
+};
+
+function geolocationError(errorObject) {
+    console.log('code: ' + errorObject.code + '\n' + 'message: ' + errorObject.message + '\n');
+}
+
+function cameraSuccess(imageData){
+    var currentLocation = JSON.parse(localStorage.getItem('currentLocation'));
+    console.log(currentLocation);
+
+    $.ajax({
+        type: "POST",
+        url: "http://206.189.1.214:5000/events/",
+        data: JSON.stringify({
+            "lat":currentLocation.lat,
+            "lon":currentLocation.lon,
+            "image": ""+imageData,
+            "input_type": 'mobile'
+        }),
+        success: function(response){
+            console.log(response);
+            swal({
+                title: 'Success',
+                text: 'Your report was succesfully sent and is currently being evaluated.',
+                confirmButtonClass: 'btn btn--stripe btn--radius blue',
+                type: 'success',
+            });
+        },
+        error: function(response){
+            console.error(response);
+            swal({
+                title: 'Error',
+                text: 'Something went wrong! Could you please try again?',
+                confirmButtonClass: 'btn btn--stripe btn--radius blue',
+                type: 'error',
+            });
+        },
+        contentType: "application/json"
+    });
+}
+
+function cameraError(errorMessage){
+    console.error(errorMessage);
+}
+
+function emergencyRequest(){
+    var currentLocation = JSON.parse(localStorage.getItem('currentLocation'));
+
+    $.ajax({
+        type: "POST",
+        url: "http://206.189.1.214:5000/events/",
+        data: JSON.stringify({
+            "lat":currentLocation.lat,
+            "lon":currentLocation.lon,
+            "input_type": 'panic'
+        }),
+        success: function(response){
+            console.log(response);
+            swal({
+                title: 'Hang in there!',
+                text: 'Emergency responders are on their way.',
+                confirmButtonClass: 'btn btn--stripe btn--radius blue',
+                type: 'success',
+            });
+        },
+        error: function(response){
+            console.error(response);
+            swal({
+                title: 'Error',
+                text: 'Something went wrong! Could you please try again?',
+                confirmButtonClass: 'btn btn--stripe btn--radius blue',
+                type: 'error',
+            });
+        },
+        contentType: "application/json"
+    });
+}
+
+function updateCurrentLocation(currentLocation){
+    
+    var previousLocation = JSON.parse(localStorage.getItem('currentLocation'));
+
+    if(
+        previousLocation.lat != currentLocation.coords.latitude ||
+        previousLocation.lon != currentLocation.coords.longitude
+    ){
+        localStorage.setItem('currentLocation', JSON.stringify({
+            "lat": currentLocation.coords.latitude,
+            "lon": currentLocation.coords.longitude,
+        }));
+    
+        
+        if(localStorage.getItem("isRegistered") != null){
+
             $.ajax({
                 type: "PUT",
-                url: "http://e2b15771.ngrok.io/users/update/" + localStorage.getItem("uuid"),
+                url: "http://206.189.1.214:5000/users/" + localStorage.getItem("uuid"),
                 data: {
-                    "phonenumber":localStorage.getItem("phonenumber"),
                     "lon":currentLocation.lon,
                     "lat":currentLocation.lat
                 },
+                success: function(response){
+                    console.log(response);
+                },
+                error: function(response){
+                    console.error(response);
+                },
                 contentType: "application/json"
             });
-            */
         }
-    };
-
-    function geolocationError(errorObject) {
-        console.log('code: ' + errorObject.code + '\n' + 'message: ' + errorObject.message + '\n');
     }
+    
+}
 
-    function cameraSuccess(imageData){
-        var currentLocation = JSON.parse(localStorage.getItem('currentLocation'));
-        console.log(currentLocation);
+function registerUser(phonenumber){
+    localStorage.setItem("phonenumber", phonenumber);
 
-        $.ajax({
-            type: "POST",
-            url: "http://206.189.1.214:5000/events/",
-            data: JSON.stringify({
-                "lat":currentLocation.lat,
-                "lon":currentLocation.lon,
-                "image": ""+imageData,
-                "input_type": 'mobile'
-            }),
-            success: function(response){
-                console.log(response);
-                swal({
-                    title: 'Success',
-                    text: 'Your report was succesfully sent and is currently being evaluated.',
-                    confirmButtonClass: 'btn btn--stripe btn--radius blue',
-                    type: 'success',
-                });
-            },
-            error: function(response){
-                console.error(response);
-                swal({
-                    title: 'Error',
-                    text: 'Something went wrong! Could you please try again?',
-                    confirmButtonClass: 'btn btn--stripe btn--radius blue',
-                    type: 'error',
-                });
-            },
-            contentType: "application/json"
-        });
-    }
+    console.log("Phonenumber: " + localStorage.getItem("phonenumber"));
+    console.log("UUID: " + localStorage.getItem("uuid"));
+    console.log("GeoLocation: " + localStorage.getItem("currentLocation"));
 
-    function cameraError(errorMessage){
-        console.error(errorMessage);
-    }
+    var currentLocation = JSON.parse(localStorage.getItem('currentLocation'));
 
-    function emergencyRequest(){
-        var currentLocation = JSON.parse(localStorage.getItem('currentLocation'));
-        console.log(currentLocation);
+    $.ajax({
+        type: "POST",
+        url: "http://206.189.1.214:5000/users/",
+        data: JSON.stringify({
+            "telephone":localStorage.getItem("phonenumber"),
+            "lon":currentLocation.lon,
+            "lat":currentLocation.lat,
+            "mobile_id": localStorage.getItem("uuid")
+        }),
+        success: function(response){
+            console.log(response);
+            swal({
+                title: 'Success',
+                text: 'You have been successfuly registered.',
+                confirmButtonClass: 'btn btn--stripe btn--radius blue',
+                type: 'success',
+            });
 
-        $.ajax({
-            type: "POST",
-            url: "http://206.189.1.214:5000/events/",
-            data: JSON.stringify({
-                "lat":currentLocation.lat,
-                "lon":currentLocation.lon,
-                "input_type": 'panic'
-            }),
-            success: function(response){
-                console.log(response);
-                swal({
-                    title: 'Hang in there!',
-                    text: 'Emergency responders are on their way.',
-                    confirmButtonClass: 'btn btn--stripe btn--radius blue',
-                    type: 'success',
-                });
-            },
-            error: function(response){
-                console.error(response);
-                swal({
-                    title: 'Error',
-                    text: 'Something went wrong! Could you please try again?',
-                    confirmButtonClass: 'btn btn--stripe btn--radius blue',
-                    type: 'error',
-                });
-            },
-            contentType: "application/json"
-        });
-    }
-
-});
+            localStorage.setItem("isRegistered", "true");
+        },
+        error: function(response){
+            console.error(response);
+            swal({
+                title: 'Error',
+                text: 'Something went wrong! Could you please try again?',
+                confirmButtonClass: 'btn btn--stripe btn--radius blue',
+                type: 'error',
+            });
+        },
+        contentType: "application/json"
+    });
+}
 
 $(document).ajaxStart(function(){
 }).ajaxComplete(function(){
@@ -136,7 +223,18 @@ $(document).ajaxStart(function(){
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() { 
+
+    localStorage.setItem('currentLocation', JSON.stringify({
+        "lat": 34.6807039,
+        "lon": 33.0430062,
+    }));
+
+    navigator.geolocation.watchPosition(geolocationSuccess, geolocationError, {
+        enableHighAccuracy: true
+    });
+
     localStorage.setItem("uuid", device.uuid);
+
     cordova.plugins.diagnostic.isGpsLocationEnabled(function(enabled){
         console.log("GPS location is " + (enabled ? "enabled" : "disabled"));
         if(!enabled){
@@ -156,73 +254,5 @@ function onDeviceReady() {
     }, function(error){
         console.error("The following error occurred: " + error);
     }); 
- }
-
- console.log(localStorage.getItem("isRegistered"));
-if(localStorage.getItem("isRegistered") == null){
-    swal({
-        title: 'Welcome to ProlepSYS',
-        text: "In order for us to inform you for a near danger we require your phone number. Would you be kind enough to provide it to us?",
-        type: 'question',
-        showCancelButton: true,
-        confirmButtonClass: 'btn btn--stripe btn--radius blue',
-        cancelButtonClass: 'btn btn--stripe btn--radius black',
-        confirmButtonText: 'Of course',
-        cancelButtonText: 'Maybe later'
-    }).then((result) => {
-        if (result.value) {
-            swal({
-                title: 'Fill it in.',
-                text: 'Type your phone number in order to receive near danger notifications',
-                input: 'tel',
-                showCancelButton: true,
-                inputPlaceholder: 'e.g +35799556677',
-                confirmButtonClass: 'btn btn--stripe btn--radius blue',
-                cancelButtonClass: 'btn btn--stripe btn--radius black',
-                allowOutsideClick: false
-        }).then((result) => {
-            localStorage.setItem("phonenumber",result.value);
-
-            console.log("Phonenumber: " + localStorage.getItem("phonenumber"));
-            console.log("UUID: " + localStorage.getItem("uuid"));
-            console.log("GeoLocation: " + localStorage.getItem("currentLocation"));
-
-            var currentLocation = JSON.parse(localStorage.getItem('currentLocation'));
-
-            $.ajax({
-                type: "POST",
-                url: "http://206.189.1.214:5000/users/",
-                data: {
-                    "telephone":localStorage.getItem("phonenumber"),
-                    "lon":currentLocation.lon,
-                    "lat":currentLocation.lat,
-                    "mobile_id": localStorage.getItem("uuid")
-                },
-                success: function(response){
-                    console.log(response);
-                    swal({
-                        title: 'Success',
-                        text: 'You have been successfuly registered.',
-                        confirmButtonClass: 'btn btn--stripe btn--radius blue',
-                        type: 'success',
-                    });
-
-                    localStorage.setItem("isRegistered", "true");
-                },
-                error: function(response){
-                    console.error(response);
-                    swal({
-                        title: 'Error',
-                        text: 'Something went wrong! Could you please try again?',
-                        confirmButtonClass: 'btn btn--stripe btn--radius blue',
-                        type: 'error',
-                    });
-                },
-                contentType: "application/json"
-            });
-
-         });
-        }
-    });         
 }
-  
+
